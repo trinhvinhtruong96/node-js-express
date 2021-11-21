381e44c47cacf7d33f121aef6dbc6982e3bf132c
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-11-21 17:19:29 +0700

    22. Setting up ESLint + Prettier in VS Code

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).send('Hello from the server sides!');
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
 });
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
